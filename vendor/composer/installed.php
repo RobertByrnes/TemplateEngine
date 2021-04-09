39357966c43c016f160a019fbd935f53c6ea3fa8
Robert Byrnes <robbyrnes@hotmail.co.uk>
2021-04-09 13:00:01 +0100

    add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7486e5e53ab1352bda665d526b29df75c840e48b',
+    'name' => 'robertbyrnes/templateengine',
+  ),
+  'versions' => 
+  array (
+    'robertbyrnes/templateengine' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7486e5e53ab1352bda665d526b29df75c840e48b',
+    ),
+  ),
+);
